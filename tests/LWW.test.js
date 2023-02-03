4cae02cccc8dded3291274b8badfeaec32fbf1e2
ben.ho <>
2023-02-03 17:49:11 +0800

    test: draft the "update" testcase

--- tests/LWW.test.js
+++ tests/LWW.test.js
@@ -5,3 +5,21 @@ const _LLWStateBased = new LLWStateBased();
 test('init payload', () => {
   expect(_LLWStateBased.query()).toStrictEqual({ "timestamp": 0, "x": null });
 });
+
+describe('update payload', () => {
+  test('success - update payload successfully with timestamp of input is larger than the timestamp of payload', () => {
+
+  });
+
+  test('failure - update payload failed with input is an empty value', () => {
+
+  });
+
+  test('failure - update payload failed with input with the invalid days timestamp', () => {
+
+  });
+
+  test('failure - update payload failed with with timestamp of input is smaller than the timestamp of payload', () => {
+
+  });
+});
